060e91d244c52a25d37a995a6d0d8b731f2c798f
Tracer16 <tracermagee@gmail.com>
2018-03-20 13:37:53 -0500

    updated images

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -5,10 +5,10 @@
 $("body").on("click", "#Rey", function() {
 
 	alert("you've selected Rey");
-	$("#Hero").html("<div class='col-md-4'>Rey<br><img class='img-responsive' src='assets/images/rey.jpg'><br><span id='heroHP'>120</span>HP</div>");
-	$("#villian1").html("<div id='enemyFinn' class='col-md-3'>Finn<br><img class='enemyImage' src='assets/images/finn.jpg'><br>100HP</div>");
-	$("#villian2").html("<div id='enemyKylo' class='col-md-3'>Kylo Ren<br><img class='enemyImage' src='assets/images/kylo.jpg'><br>180HP</div>");
-	$("#villian3").html("<div id='enemyPhasma' class='col-md-3'>Captain Phasma<br><img class='enemyImage' src='assets/images/phasma.jpg'><br>150HP</div>");
+	$("#Hero").html("<div class='col-md-4'>Rey<br><img class='img-responsive' src='assets/images/rey.png'><br><span id='heroHP'>120</span>HP</div>");
+	$("#villian1").html("<div id='enemyFinn' class='col-md-3'>Finn<br><img class='enemyImage' src='assets/images/finn.png'><br>100HP</div>");
+	$("#villian2").html("<div id='enemyKylo' class='col-md-3'>Kylo Ren<br><img class='enemyImage' src='assets/images/kylo.png'><br>180HP</div>");
+	$("#villian3").html("<div id='enemyPhasma' class='col-md-3'>Captain Phasma<br><img class='enemyImage' src='assets/images/phasma.png'><br>150HP</div>");
 	$("#characterSelect").html("<div class='blankRow'></div>");
 
 })
@@ -16,30 +16,30 @@ $("body").on("click", "#Rey", function() {
 .on("click", "#Finn", function() {
 
 	alert("you've selected Finn");
-	$("#Hero").html("<div class='col-md-4'>Finn<br><img class='img-responsive' src='assets/images/finn.jpg'><br><span id='heroHP'>100</span>HP</div>");
-	$("#villian4").html("<div id='enemyRey' class='col-md-3'>Rey<br><img class='enemyImage' src='assets/images/rey.jpg'><br>120HP</div>");
-	$("#villian2").html("<div id='enemyKylo' class='col-md-3'>Kylo Ren<br><img class='enemyImage' src='assets/images/kylo.jpg'><br>180HP</div>");
-	$("#villian3").html("<div id='enemyPhasma' class='col-md-3'>Captain Phasma<br><img class='enemyImage' src='assets/images/phasma.jpg'><br>150HP</div>");
+	$("#Hero").html("<div class='col-md-4'>Finn<br><img class='img-responsive' src='assets/images/finn.png'><br><span id='heroHP'>100</span>HP</div>");
+	$("#villian4").html("<div id='enemyRey' class='col-md-3'>Rey<br><img class='enemyImage' src='assets/images/rey.png'><br>120HP</div>");
+	$("#villian2").html("<div id='enemyKylo' class='col-md-3'>Kylo Ren<br><img class='enemyImage' src='assets/images/kylo.png'><br>180HP</div>");
+	$("#villian3").html("<div id='enemyPhasma' class='col-md-3'>Captain Phasma<br><img class='enemyImage' src='assets/images/phasma.png'><br>150HP</div>");
 	$("#characterSelect").html("<div class='blankRow'></div>");
 })
 
 .on("click", "#Kylo", function() {
 
 	alert("you've selected Kylo Ren");
-	$("#Hero").html("<div class='col-md-4'>Kylo Ren<br><img class='img-responsive' src='assets/images/kylo.jpg'><br><span id='heroHP'>180</span>HP</div>");
-	$("#villian1").html("<div id='enemyFinn' class='col-md-3'>Finn<br><img class='enemyImage' src='assets/images/finn.jpg'><br>100HP</div>");
-	$("#villian4").html("<div id='enemyRey' class='col-md-3'>Rey<br><img class='enemyImage' src='assets/images/rey.jpg'><br>120HP</div>");
-	$("#villian3").html("<div id='enemyPhasma' class='col-md-3'>Captain Phasma<br><img class='enemyImage' src='assets/images/phasma.jpg'><br>150HP</div>");
+	$("#Hero").html("<div class='col-md-4'>Kylo Ren<br><img class='img-responsive' src='assets/images/kylo.png'><br><span id='heroHP'>180</span>HP</div>");
+	$("#villian1").html("<div id='enemyFinn' class='col-md-3'>Finn<br><img class='enemyImage' src='assets/images/finn.png'><br>100HP</div>");
+	$("#villian4").html("<div id='enemyRey' class='col-md-3'>Rey<br><img class='enemyImage' src='assets/images/rey.png'><br>120HP</div>");
+	$("#villian3").html("<div id='enemyPhasma' class='col-md-3'>Captain Phasma<br><img class='enemyImage' src='assets/images/phasma.png'><br>150HP</div>");
 	$("#characterSelect").html("<div class='blankRow'></div>");
 })
 
 .on("click", "#Phasma", function() {
 
 	alert("you've selected Captain Phasma");
-	$("#Hero").html("<div class='col-md-4'>Captain Phasma<br><img class='img-responsive' src='assets/images/phasma.jpg'><br><span id='heroHP'>150</span>HP</div>");
-	$("#villian1").html("<div id='enemyFinn' class='col-md-3'>Finn<br><img class='enemyImage' src='assets/images/finn.jpg'><br>100HP</div>");
-	$("#villian2").html("<div id='enemyKylo' class='col-md-3'>Kylo Ren<br><img class='enemyImage' src='assets/images/kylo.jpg'><br>180HP</div>");
-	$("#villian4").html("<div id='enemyRey' class='col-md-3'>Rey<br><img class='enemyImage' src='assets/images/rey.jpg'><br>120HP</div>");
+	$("#Hero").html("<div class='col-md-4'>Captain Phasma<br><img class='img-responsive' src='assets/images/phasma.png'><br><span id='heroHP'>150</span>HP</div>");
+	$("#villian1").html("<div id='enemyFinn' class='col-md-3'>Finn<br><img class='enemyImage' src='assets/images/finn.png'><br>100HP</div>");
+	$("#villian2").html("<div id='enemyKylo' class='col-md-3'>Kylo Ren<br><img class='enemyImage' src='assets/images/kylo.png'><br>180HP</div>");
+	$("#villian4").html("<div id='enemyRey' class='col-md-3'>Rey<br><img class='enemyImage' src='assets/images/rey.png'><br>120HP</div>");
 	$("#characterSelect").html("<div class='blankRow'></div>");
 })
 
@@ -48,7 +48,7 @@ $("body").on("click", "#Rey", function() {
 .on("click", "#enemyKylo", function() {
 
 	alert("Prepare to battle Kylo Ren");
-	$("#defender").html("<div class='col-md-4'>Kylo Ren<br><img class='img-responsive' src='assets/images/kylo.jpg'><br><span id='villianHP'>180</span>HP</div>");
+	$("#defender").html("<div class='col-md-4'>Kylo Ren<br><img class='img-responsive' src='assets/images/kylo.png'><br><span id='villianHP'>180</span>HP</div>");
 	$("#villian2").html("<div></div>");
 
 })
@@ -56,7 +56,7 @@ $("body").on("click", "#Rey", function() {
 .on("click", "#enemyRey", function() {
 
 	alert("Prepare to battle Rey");
-	$("#defender").html("<div class='col-md-4'>Rey<br><img class='img-responsive' src='assets/images/rey.jpg'><br><span id='villianHP value='120'>120</span>HP</div>");
+	$("#defender").html("<div class='col-md-4'>Rey<br><img class='img-responsive' src='assets/images/rey.png'><br><span id='villianHP value='120'>120</span>HP</div>");
 	$("#villian4").html("<div></div>");
 	// var villianHP = 120;
 	$("#attackBtn").click(function(){
@@ -68,14 +68,14 @@ $("body").on("click", "#Rey", function() {
 .on("click", "#enemyFinn", function() {
 
 	alert("Prepare to battle Finn");
-	$("#defender").html("<div class='col-md-4'>Finn<br><img class='img-responsive' src='assets/images/finn.jpg'><br><span id='villianHP'>100</span>HP</div>");
+	$("#defender").html("<div class='col-md-4'>Finn<br><img class='img-responsive' src='assets/images/finn.png'><br><span id='villianHP'>100</span>HP</div>");
 	$("#villian1").html("<div></div>");
 })
 
 .on("click", "#enemyPhasma", function() {
 
 	alert("Prepare to battle Captain Phasma");
-	$("#defender").html("<div class='col-md-4'>Captain Phasma<br><img class='img-responsive' src='assets/images/phasma.jpg'><br><span id='villianHP'>150</span>HP</div>");
+	$("#defender").html("<div class='col-md-4'>Captain Phasma<br><img class='img-responsive' src='assets/images/phasma.png'><br><span id='villianHP'>150</span>HP</div>");
 	$("#villian3").html("<div></div>");
 })
 
